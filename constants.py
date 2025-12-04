dc8e35ec35d5f41caa9f653f8e5e0b2b9c947e24
Carlosss2 <147357485+Carlosss2@users.noreply.github.com>
2025-12-04 09:53:59 -0600

    arreglando errores

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 import re
 from pathlib import Path
 
-# Regex para identificador: solo minúsculas, sin guion bajo
-REGEX_IDENT_PATTERN = r'^[a-z](?:[a-z0-9]{1,}|[a-z0-9]{0,})$'
+# 
+REGEX_IDENT_PATTERN = r'^HOLA[a-z]+$'
 REGEX_IDENT = re.compile(REGEX_IDENT_PATTERN)
 
-# Paths por defecto (carpeta del proyecto Practica1)
+# Paths por defecto 
 BASE_DIR = Path(__file__).parent
 DEFAULT_DICC = str(BASE_DIR / "diccionario.txt")
 DEFAULT_INPUT = str(BASE_DIR / "texto_entrada.txt")
